9540f5f2ee8d91bde753bd9ce342ebee434d9fb0
Nico van Biljon <141059907+nicovbiljon2@users.noreply.github.com>
2026-07-21 19:08:40 +0200

    feat: add edit functionality and list indexing support
    - Add edit functionality to the main program
    - Implement list indexing for item updates
    - Add bonus3.py to practice for loops
    - Add bonus4.py to practice string updates using indexing
    - Demonstrate replacing "." with "-" in strings

--- todo_app/main.py
+++ todo_app/main.py
@@ -1,7 +1,7 @@
 todos = [] # create an empty list
 
 while True:
-    user_action = input("Type add, show or exit: ")
+    user_action = input("Type add, show, edit or exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -12,6 +12,11 @@ while True:
             for item in todos:
                 item = item.title()
                 print(item)
+        case "edit":
+            number = int(input("Enter the number of the todo to edit: "))
+            number = number -1
+            new_todo = input("Enter a new todo: ")
+            todos[number] = new_todo
         case "exit":
             break
 
